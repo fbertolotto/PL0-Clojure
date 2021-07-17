a63c83b38feb869a3e4204131e39f5c1f0edf990
Francisco Bertolotto <fbertolotto@fi.uba.ar>
2021-07-17 17:47:20 -0300

    refactor a-mayusculas-salvo-strings

--- tp/src/tp/core.clj
+++ tp/src/tp/core.clj
@@ -700,21 +700,18 @@
 ; "  WRITELN ('Se ingresa un valor, se muestra su doble.');"
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn capitalize? [string substring]
-  (if (re-find #"'.*'" string)
-    (if ( = (compare (second (clojure.string/split (re-find #"'.*'" string) #"'")) substring) 0) ; si existe subcadena se compara con la recibida
-      ; (re-find #"'.*'" string) devuelve la subcadena con los ' '
-      ; (clojure.string/split anterior #"'") devuelve la cadena anterior sin los ' '
-      ; (second anterior) se utiliza second porque devuelve una secuencia con dos elementos y el primero es vacio
-      ; finalmente se compara con la subcadena para ver si la diferncia es 0
-      (re-find #"'.*'" string) ; si coinciden no se capitaliza y se devuelve con los ' ' (devolver substring no los incluye)
-      (clojure.string/upper-case substring) ; si no coincide se devuelve capitalizada la substring
-    )
-    (clojure.string/upper-case substring) ; si no existe subcadena, se capitaliza
+  (if (= string substring)
+    (str (symbol "'") substring (symbol "'"))
+    (clojure.string/upper-case substring)
   )
 )
-
 (defn a-mayusculas-salvo-strings [s]
-  (clojure.string/join (map (partial capitalize? s) (clojure.string/split s #"'")))
+  (if (re-find #"'.*'" s)
+    (let [string (second (clojure.string/split (re-find #"'.*'" s)  #"'"))] ;el second es split porque devuelve ["" "string"]
+      (clojure.string/join (map (partial capitalize? string) (clojure.string/split s #"'")))
+    )
+    (clojure.string/upper-case s)
+  )
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
